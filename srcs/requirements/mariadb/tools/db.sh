54198ff90c112bba4d616e1bd5edb516263d2d7b
Soufiane El Bouazzaoui <soufianbouazzaoui03@soufianbouazzaoui03@gmail.com>
2025-07-02 18:27:13 +0100

    add wp nginx compose

--- srcs/requirements/mariadb/tools/db.sh
+++ srcs/requirements/mariadb/tools/db.sh
@@ -1,8 +1,17 @@
+# service mysql start
+# mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABSE}\`;"
+# mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+# mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABSE}\`.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+# mysql -e "FLUSH PRIVILEGES;"
+# mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+# exec mysqld_safe
+
+
 service mysql start
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABSE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABSE}\`.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "CREATE DATABASE IF NOT EXISTS \`DB\`;"
+mysql -e "CREATE USER IF NOT EXISTS 'soel-bou'@'%' IDENTIFIED BY '0000';"
+mysql -e "GRANT ALL PRIVILEGES ON \`DB\`.* TO 'soel-bou'@'%';"
 mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-exec mysqld_safe
+mysqladmin -u root -p0000 shutdown
+# exec mysqld_safe
---
+++ srcs/requirements/wordpress/tools/wp-entrypoint.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -e
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+mkdir -p /var/www/html
+cd /var/www/html
+
+chown -R www-data:www-data /var/www/html
+chmod -R 755 /var/www/html/
+
+if [ ! -f wp-load.php ]; then
+  wp core download --allow-root
+fi
+
+if [ ! -f wp-config.php ]; then
+  wp config create \
+    --dbname="$MARIADB_DATABASE" \
+    --dbuser="$MARIADB_USER" \
+    --dbpass="$MARIADB_PASSWORD" \
+    --dbhost=mariadb:3306 \
+    --allow-root
+fi
+
+if ! wp core is-installed --allow-root; then
+  wp core install \
+    --url="$DOMAIN_NAME" \
+    --title="$WP_TITLE" \
+    --admin_user="$WP_ADMIN_USERNAME" \
+    --admin_password="$WP_ADMIN_PASSWORD" \
+    --admin_email="$WP_ADMIN_EMAIL" \
+    --allow-root
+fi
+
+if ! wp user get "$WP_USER_USERNAME" --field=ID --allow-root &> /dev/null; then
+  wp user create \
+    "$WP_USER_USERNAME" "$WP_USER_EMAIL" \
+    --user_pass="$WP_USER_PASSWORD" \
+    --role="$WP_USER_ROLE" \
+    --allow-root
+fi
+
+sed -i 's|^listen = .*|listen = 9000|' /etc/php/7.4/fpm/pool.d/www.conf
+mkdir -p /run/php
+
+exec /usr/sbin/php-fpm7.4 -F
