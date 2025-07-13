a2613919539ac4fd4349b722ab5fc99af9ffffdd
Soufiane El Bouazzaoui <soufianbouazzaoui03@soufianbouazzaoui03@gmail.com>
2025-07-13 18:28:12 +0100

    done

--- srcs/requirements/mariadb/tools/db.sh
+++ srcs/requirements/mariadb/tools/db.sh
@@ -13,7 +13,7 @@ echo "Creating database: ${SQL_DATABASE}"
 echo "Creating user: ${SQL_USER}"
 
 # Run all SQL commands in a single clean block
-mysql -uroot <<EOF
+mysql -uroot -p${SQL_ROOT_PASSWORD} <<EOF
 CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;
 CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';
--- srcs/requirements/wordpress/tools/wp-entrypoint.sh
+++ srcs/requirements/wordpress/tools/wp-entrypoint.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-set -e
-
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
 mkdir -p /var/www/html
 cd /var/www/html
-
 chown -R www-data:www-data /var/www/html
 chmod -R 755 /var/www/html/
 
@@ -29,13 +26,13 @@ if ! wp core is-installed --allow-root; then
   wp core install \
     --url="$DOMAIN_NAME" \
     --title="$WP_TITLE" \
-    --admin_user="$WP_ADMIN_USERNAME" \
+    --admin_user="$WP_ADMIN_USER" \
     --admin_password="$WP_ADMIN_PASSWORD" \
     --admin_email="$WP_ADMIN_EMAIL" \
     --allow-root
 fi
 
-if ! wp user get "$WP_USER_USERNAME" --field=ID --allow-root &> /dev/null; then
+if ! wp user get "$WP_USERNAME" --field=ID --allow-root &> /dev/null; then
   wp user create \
     "$WP_USERNAME" "$WP_USER_EMAIL" \
     --user_pass="$WP_USER_PASSWORD" \
@@ -47,3 +44,4 @@ sed -i 's|^listen = .*|listen = 9000|' /etc/php/7.4/fpm/pool.d/www.conf
 mkdir -p /run/php
 
 exec /usr/sbin/php-fpm7.4 -F
+
